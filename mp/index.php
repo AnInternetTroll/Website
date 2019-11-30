b600cb4e911ed6988d0b494879a38cc7d2b8f486
An Internet Troll <lucafulger@gmail.com>
2019-11-30 16:20:27 +0100

    Removed IP stealing
    Not actually, just commented it.

--- mp/index.php
+++ mp/index.php
@@ -11,18 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- -->
-    <!-- <script type="text/javascript">
-    var ip = new 
-  $.getJSON("https://api.ipify.org/?format=json", function(e) {
-    console.log(e.ip);
-});
-  </script>-->
-    <?php
-$file = "../ipaddresses.txt"; //turns the text file into a variable
-$f=fopen($file, 'a+'); //opens up the text file for reading and writing
-fwrite($f,"\n--"."Date:".date("l jS \of F Y h:i:s A")."\nIP: ".$_SERVER['REMOTE_ADDR']."\nHOSTNAME: ".gethostbyaddr($_SERVER['REMOTE_ADDR'])."\n"); //writes the IP address to ipaddresses.txt
-fclose($f); //closes ipaddresses.txt for reading and writing
-?>
+<?php include '../include/ip.php';?>
     <link rel="stylesheet" type="text/css" href="../include/main.css">
     <style type="text/css">
     li .kilder {
