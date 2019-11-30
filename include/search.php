7d85bc66f0c9ce8d3c3e1a7ced2a3ea6a6235ea0
An Internet Troll <lucafulger@gmail.com>
2019-11-30 16:38:58 +0100

    Update search.php

--- include/search.php
+++ include/search.php
@@ -1,9 +1,7 @@
 <?php
 
-$norsk = file_get_contents('http://37.200.47.164/norsk/');//IMAGINE THIS IS REALLY HOME.PHP
-$memes = file_get_contents('http://37.200.47.164/memes/');
-$norskurl = "http://37.200.47.164/norsk/";
-$memesurl = "http://37.200.47.164/memes/";//JUST HERE TO SHOW AN EXAMPLE
+$norsk = file_get_contents('../norsk/index.php');//IMAGINE THIS IS REALLY HOME.PHP
+$norskurl = "../norsk/index.php";
 
 function get_title($url){
   $str = file_get_contents($url);
@@ -25,17 +23,7 @@ if (stripos($norsk, $search) !== false) {//USING EXAMPLE.COM TO SHOW IT WORKS
     echo '<h2><a href="'.$norskurl.'">'.get_title($norskurl).'</a></h2>';
 }
 
-function get_title($url){
-  $str = file_get_contents($url);
-  if(strlen($str)>0){
-    $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-    preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
-    return $title[1];
-  }
-}
 
-if (stripos($memes, $search) !== false) {//USING EXAMPLE.COM TO SHOW IT WORKS
-    echo '<h2><a href="'.$memesurl.'">'.get_title($memesurl).'</a></h2>';
-}
+
 
 ?>
