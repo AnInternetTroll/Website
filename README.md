2abb84152c8f19ac49f874ee839812bdfe98395e
root <lucafulger@gmail.com>
2020-02-21 09:54:41 +0000

    Added dark-toggle to package.json

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Luca's website
 This is a repository to document the growth of my hobby. Feel free to take code and use it as you will. 
-
-## Dependencies
-* 
--- about/index.php
+++ about/index.php
@@ -5,13 +5,13 @@ include $root . '/Website/include/php_stuff.php';?>
 
 <head>
 
-    <meta name="og:title" name="og:title" content="TEMPLATE"/>
-    <meta name="og:description" property="og:description" content="description" />
+    <meta name="og:title" name="og:title" content="About me"/>
+    <meta name="og:description" property="og:description" content="The description of the maker of this website" />
     <meta name="og:locale" property="og:locale" content="en_GB" />
-    <meta name="og:site_name" property="og:site_name" content="site name, like the title" />
+    <meta name="og:site_name" property="og:site_name" content="Luca's website" />
     <meta name="og:image" property="og:image" content="/Website/media/dickbutt.png" />
-    <meta name="article" property="article" content="/Website/home/article.md" />
-    <meta name="og:url" content="/Website/home/" />
+    <meta name="article" property="article" content="/Website/about/article.md" />
+    <meta name="og:url" content="/Website/about/" />
     <link rel="manifest" href="/Website/include/manifest.json">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -20,30 +20,16 @@ include $root . '/Website/include/php_stuff.php';?>
     <title>Home</title>
 
     <?php include $root . "/Website/include/includes.php";?>
-
-
-    <style type="text/css">
-    .article {
-        position: static;
-        line-height: 1.6;
-        font-size: 15px;
-    }
-    </style>
     <?php include $root . "/Website/include/nav.php";?>
-    <!-- End of Navbar -->
-    <!-- Article -->
 </head>
 <body>
     <br />
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-
-
-
-<?php include $root . "/Website/include/article.php";?>
+				<?php include $root . "/Website/include/article.php";?>
                     <br />
-<?php include $root . "/Website/include/comments_html.php"?>
+				<?php include $root . "/Website/include/comments_html.php"?>
                 </div>
             </div>
         </div>
