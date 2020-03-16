d6c776f1b69ba4b08cc54b8668886147d61326c9
root <lucafulger@gmail.com>
2020-03-16 16:33:12 +0000

    modified:   .gitignore 	modified:   include/includes.php 	modified:   include/main.js 	modified:   include/manifest.json 	renamed:    include/sw.js -> sw.js

--- include/includes.php
+++ include/includes.php
@@ -1,4 +1,5 @@
     <meta charset="utf-8">
+    <meta name="theme-color" content="#007bff"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
     <link rel="icon" href="/Website/media/dickbutt_192.png">
--- include/main.js
+++ include/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     // Register a service worker hosted at the root of the
     // site using the default scope.
-    navigator.serviceWorker.register('/Website/include/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/Website/sw.js').then(function(registration) {
       console.log('Service worker registration succeeded:', registration);
     }, /*catch*/ function(error) {
       console.log('Service worker registration failed:', error);
